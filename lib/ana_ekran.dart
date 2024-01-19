499b4bcbb68c3f2267d2847cdaeea8ef836b4b5a
Lutfi <lutficelik05@gmail.com>
2024-01-19 04:04:36 +0300

    3. commit

--- lib/ana_ekran.dart
+++ lib/ana_ekran.dart
@@ -1,35 +1,113 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'liste_hazirla.dart';
+import 'personeller.dart';
+import 'gun_ayari.dart';
 
-void main() => runApp(MyApp());
+class AnaEkran extends StatefulWidget {
+  @override
+  _AnaEkranState createState() => _AnaEkranState();
+}
 
-class MyApp extends StatelessWidget {
+class _AnaEkranState extends State<AnaEkran> {
+  int kayitliPersonelSayisi = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _kayitliPersonelSayisiniGetir();
+  }
+
+  void _kayitliPersonelSayisiniGetir() async {
+    final prefs = await SharedPreferences.getInstance();
+    final kayitliPersoneller = prefs.getStringList('kayitliPersoneller') ?? [];
+    setState(() {
+      kayitliPersonelSayisi = kayitliPersoneller.length;
+    });
+  }
+
+  Widget _buildButton(BuildContext context, String text, IconData icon, VoidCallback onPressed) {
+    return InkWell(
+      onTap: onPressed,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(icon, size: 48.0),
+          Text(
+            text,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtonWithCounter(BuildContext context, String text, IconData icon, int counter, VoidCallback onPressed) {
+    return InkWell(
+      onTap: onPressed,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(icon, size: 48.0),
+          Text(
+            text,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(height: 4),
+          Text(
+            '$counter kayıtlı personel',
+            style: TextStyle(
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      home: AnaEkran(),
+    // Ekran boyutuna göre crossAxisCount değerini belirle
+    int crossAxisCount = MediaQuery.of(context).size.width > 600 ? 3 : 2;
+
+    // Ekran boyutuna göre childAspectRatio değerini belirle
+    double childAspectRatio = (MediaQuery.of(context).size.width / crossAxisCount) / (MediaQuery.of(context).size.height / (crossAxisCount + 1));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ana Ekran'),
+      ),
+      body: GridView.count(
+        crossAxisCount: crossAxisCount,
+        childAspectRatio: childAspectRatio,
+        mainAxisSpacing: 8.0,
+        crossAxisSpacing: 8.0,
+        padding: EdgeInsets.all(8.0),
+        children: <Widget>[
+          _buildButton(context, 'Liste Tarihi Seç', Icons.date_range, () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) => ListeHazirla()));
+          }),
+          _buildButtonWithCounter(context, 'Personel Listesi', Icons.list, kayitliPersonelSayisi, () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) => Personeller())).then((_) => _kayitliPersonelSayisiniGetir());
+          }),
+          _buildButton(context, 'Personel Gün Ayarı', Icons.calendar_today, () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) => GunAyari()));
+          }),
+          _buildButton(context, 'Buton 4', Icons.add_circle_outline, () {
+            // Buton 4'e basıldığında yapılacak işlem
+          }),
+        ],
+      ),
     );
   }
 }
 
-class AnaEkran extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Nöbetmatik'),
-      ),
-      child: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(child: Center(child: CupertinoButton(child: Text('Buton1'), onPressed: () {}))),
-            Expanded(child: Center(child: CupertinoButton(child: Text('Buton2'), onPressed: () {}))),
-            Expanded(child: Center(child: CupertinoButton(child: Text('Buton3'), onPressed: () {}))),
-            Expanded(child: Center(child: CupertinoButton(child: Text('Buton4'), onPressed: () {}))),
-          ],
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(MaterialApp(home: AnaEkran()));
